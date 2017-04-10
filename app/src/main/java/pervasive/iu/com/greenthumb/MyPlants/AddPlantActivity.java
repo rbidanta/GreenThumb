03ed30161af7071c06c935c5e1739c3ec60ea32b
madrinathapa <madrinathapa.rs@gmail.cm>
2017-04-10 01:13:26 -0400

    Add plants
    Adding new plant and binding the list view with existing plants.

--- app/src/main/java/pervasive/iu/com/greenthumb/MyPlants/AddPlantActivity.java
+++ app/src/main/java/pervasive/iu/com/greenthumb/MyPlants/AddPlantActivity.java
@@ -1,30 +1,42 @@
 package pervasive.iu.com.greenthumb.MyPlants;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.graphics.drawable.Drawable;
 
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import pervasive.iu.com.greenthumb.Login.LoginActivity;
 import pervasive.iu.com.greenthumb.Model.Plants;
@@ -35,27 +47,28 @@ public class AddPlantActivity extends AppCompatActivity implements View.OnClickL
     private FirebaseAuth firebaseAuth;
     private DatabaseReference dbreference;
     private ImageButton buttonSave;
-    private FirebaseStorage storage = FirebaseStorage.getInstance(); 
-    private StorageReference plantStorageRef = storage.getReference("plants"); 
-    private StorageReference plantImagesRef;  
-    //private EditText gardenName, gardenAddress; 
-    //private TextInputLayout inputLayoutgName, inputLayoutgAddress; 
-    //private saveInfo loggedInUserInfo = new saveInfo();   
-    //private ImageView mImageView;  
-    private Uri gardenImageUri = null;   
-    private String mCurrentPhotoPath;  
-    //private static final String JPEG_FILE_PREFIX = "IMG_"; 
-   // private static final String JPEG_FILE_SUFFIX = ".jpg";  
-   // private AlbumStorageDirFactory mAlbumStorageDirFactory = null; 
+    private ImageView plantImg;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference plantStorageRef = storage.getReference("plants");
+    private StorageReference plantImagesRef;
+    private Uri gardenImageUri = null;
+    private String mCurrentPhotoPath;
+    /*private static final String JPEG_FILE_PREFIX = "IMG_"; 
+    private static final String JPEG_FILE_SUFFIX = ".jpg";  
+   private AlbumStorageDirFactory mAlbumStorageDirFactory = null; */
     private EditText editTextPlantName;
     private EditText editTextTime;
+    private EditText editTextKitId;
+    private RadioGroup radioGroupLoc;
+    private RadioButton radioBtnSelected;
     private RadioButton radioBtnIn;
     private RadioButton radioBtnOut;
+    private TextInputLayout inputLayoutPlantName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_plant);
+        setContentView(R.layout.activity_add_plant);
         firebaseAuth = FirebaseAuth.getInstance();
 
         if(firebaseAuth.getCurrentUser()==null)
@@ -65,102 +78,222 @@ public class AddPlantActivity extends AppCompatActivity implements View.OnClickL
         }
 
         dbreference = FirebaseDatabase.getInstance().getReference();
-        buttonSave = (ImageButton) findViewById(R.id.btnSave);
-        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rgLocation);
-        radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-            }
-        });
+        buttonSave = (ImageButton) findViewById(R.id.btnSavePlant);
+        plantImg = (ImageView) findViewById(R.id.imgViewPlant);
+        editTextPlantName = (EditText) findViewById(R.id.txtPlantName);
+        editTextTime = (EditText) findViewById(R.id.txtNotiTime);
+        editTextKitId = (EditText) findViewById(R.id.txtKitNum);
+        radioGroupLoc = (RadioGroup) findViewById(R.id.rgLocation);
+        radioBtnIn = (RadioButton) findViewById(R.id.rbIndoor);
+        radioBtnOut = (RadioButton) findViewById(R.id.rbOutdoor);
+        //inputLayoutPlantName = (TextInputLayout) findViewById(R.id.inputPlantName);
         buttonSave.setOnClickListener(this);
-      /*  radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-
-                View radioButton = radioGroup.findViewById(checkedId);
-                int index = radioGroup.indexOfChild(radioButton);
-
-                // Add logic here
-
-                switch (index) {
-                    case 0: // first button
-
-                        Toast.makeText(getApplicationContext(), "Selected button number " + index, 500).show();
-                        break;
-                    case 1: // secondbutton
-
-                        Toast.makeText(getApplicationContext(), "Selected button number " + index, 500).show();
-                        break;
-                }
-            }*/
-        //radioBtnIn = (RadioButton) findViewById(R.id.rbIndoor);
-        //radioBtnIn.setOnClickListener();
-        //radioBtnOut = (RadioButton) findViewById(R.id.rbOutdoor);
-
-        //  rb.setOnClickListener(first_radio_listener);
         OnClickListener first_radio_listener = new OnClickListener() {
             public void onClick(View v) {
                 if (radioBtnIn.isChecked()) {
                 }
             }
-
-            ;
         };
     }
     public void savePlant(){
-        long kitId = 12434;
-        String plantName = "Rose";
-        String imageName ="sdha";
-        String location = "Indoor";
-        int notificationTime = 20;
-        Date dNow = new Date( );
-        Date lastModified = dNow ;
+        System.out.println("Inside savePlant");
+
+        dbreference = FirebaseDatabase.getInstance().getReference("Plants");
+        String plantId = dbreference.push().getKey();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        String userId = user.getUid();
+        String kitId = editTextKitId.getText().toString().trim();
+        String plantName = editTextPlantName.getText().toString().trim();
+        String notificationTime = editTextTime.getText().toString().trim();
+        // get selected radio button from radioGroup
+        int selectedId = radioGroupLoc.getCheckedRadioButtonId();
+
+        String location;
+
+        radioBtnSelected = (RadioButton) findViewById(selectedId);
+
+        if(radioBtnSelected == radioBtnIn){
+            location = "Indoor";
+        }else{
+            location = "Outdoor";
+        }
+
+        Date lastModified = new Date();
+        plantImagesRef = plantStorageRef.child(plantId+"/"+plantName.replaceAll(" ","")+".jpg");
         HashMap<String, String> val = new HashMap<String, String>();
         val.put("moisture", "Twenty One");
+        val.put("temp","18.5");
+        val.put("ph", "5.5");
         HashMap<String, String> thresholdValues = val;
-        Plants plant = new Plants(kitId,plantName, imageName,location,notificationTime,lastModified,thresholdValues);
-        FirebaseUser user=firebaseAuth.getCurrentUser();
 
-        dbreference.child(user.getUid()).setValue(plant);
+        Plants plant = new Plants(plantId, kitId, userId, plantName, location, notificationTime, lastModified, thresholdValues);
+        dbreference.child(plantId).setValue(plant);
+        putImagetoFireBase();
     }
 
     @Override
     public void onClick(View v) {
         if(v==buttonSave)
         {
+            if(!validatePlantName()){
+                return;
+            }
             savePlant();
+
+            Toast.makeText(this,"Plant information has been saved successfully",Toast.LENGTH_LONG).show();
+
+            finish();
+
+        }
+    }
+
+    private boolean validatePlantName() {
+        if (editTextPlantName.getText().toString().trim().isEmpty()) {
+            inputLayoutPlantName.setError(getString(R.string.err_msg_gname));
+            requestFocus(editTextPlantName);
+            return false;
+        } else {
+            inputLayoutPlantName.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    public void onClickCamera(View v){
+
+        System.out.println("Inside onClickCamera");
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            System.out.println("Inside onClickCamera 1");
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.greenthumb.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
 
     }
-   /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
 
-        Drawable drawablecart = menu.getItem(0).getIcon();
-        drawablecart.mutate();
-        drawablecart.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+    private void setPic() {
 
-        return super.onCreateOptionsMenu(menu);
+        System.out.println("Inside Set Pic");
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+        int targetW = plantImg.getWidth();
+        int targetH = plantImg.getHeight();
+
+		/* Get the size of the image */
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+		/* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+
+		/* Set bitmap options to scale the image decode target */
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        //bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap */
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+		/* Associate the Bitmap to the ImageView */
+        plantImg.setImageBitmap(bitmap);
+        //mVideoUri = null;
+        plantImg.setVisibility(View.VISIBLE);
+        //mVideoView.setVisibility(View.INVISIBLE);
+    }
+
+    private File createImageFile() throws IOException {
+
+        System.out.println("Inside createImageFile");
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        System.out.println("mCurrentPhotoPath"+mCurrentPhotoPath);
+        return image;
+    }
+
+    private void putImagetoFireBase(){
+        plantImg.setDrawingCacheEnabled(true);
+        plantImg.buildDrawingCache();
+        Bitmap bitmap = plantImg.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBdata = baos.toByteArray();
+
+        UploadTask uploadTask = plantImagesRef.putBytes(imageBdata);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+            }
+        });
+    }
+    private void handleBigCameraPhoto() {
+        System.out.println("handleBigCameraPhoto");
+
+        if (mCurrentPhotoPath != null) {
+            setPic();
+            //galleryAddPic();
+            mCurrentPhotoPath = null;
+        }
+
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if(id == R.id.shopping_cart){
+        if (resultCode == RESULT_OK) {
 
-            // code to open the shopping cart.
-            Intent intent = new Intent(SearchActivity.this, ShoppingCartActivity.class);
-            startActivity(intent);
-
-            return true;
-
+            System.out.println("Inside Activity Result");
+            handleBigCameraPhoto();
         }
 
-        return super.onOptionsItemSelected(item);
-    }*/
+    }
 }
--- app/src/main/java/pervasive/iu/com/greenthumb/MyPlants/plant.java
+++ app/src/main/java/pervasive/iu/com/greenthumb/MyPlants/plant.java
@@ -4,12 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import pervasive.iu.com.greenthumb.GardenPartner.GardenOverview;
 import pervasive.iu.com.greenthumb.R;
 
 /**
@@ -18,18 +35,24 @@ import pervasive.iu.com.greenthumb.R;
 
 public class plant extends Fragment {
     private ImageButton btnAddPlant;
+    private ListView lv;
+    private List<String> plantList;
+    private DatabaseReference plantReference;
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-       // btnAddPlant = (ImageButton) getView().findViewById(R.id.btnAdd);
         getActivity().setTitle("My plant");
+        plantReference = FirebaseDatabase.getInstance().getReference("Plants");
+
+        lv = (ListView) view.findViewById(R.id.plantList);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return inflater.inflate(R.layout.plant,container,false);
-        View view = inflater.inflate(R.layout.plant,container,false);
+        View view = inflater.inflate(R.layout.activity_main_plant, container, false);
 
         btnAddPlant = (ImageButton) view.findViewById(R.id.btnAdd);
 
@@ -42,13 +65,67 @@ public class plant extends Fragment {
 
         return view;
     }
-   /* @Override
-    public void onClick(View v) {
-        Toast toast = Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT);
-        toast.show();
-        if(v == btnAddPlant) {
-            //getActivity().finish();
-            startActivity(new Intent(getActivity(), AddPlantActivity.class));
-        }
-    }*/
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        plantReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                plantList = new ArrayList<String>();
+
+                for (DataSnapshot gardenSnapshot : dataSnapshot.getChildren()) {
+                    Map<String, Object> gInfoMap = (HashMap<String, Object>) gardenSnapshot.getValue();
+
+                    plantList.add(gInfoMap.get("plantName").toString());
+                }
+
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                        getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        plantList);
+                lv.setAdapter(arrayAdapter);
+
+
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        String name = (String) parent.getItemAtPosition(position);
+
+                        Fragment frag = new GardenOverview();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("gardenName", name);
+                        frag.setArguments(bundle);
+
+                        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                        transaction.replace(R.id.content_navigation, frag); // give your fragment container id in first parameter
+                        transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
+                        transaction.commit();
+                    }
+                });
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void onOptionsMenuClosed(Menu menu) {
+        super.onOptionsMenuClosed(menu);
+
+    }
 }
