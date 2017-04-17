f2e140db156b2de05a6d8cf9bec84f53c6f95c99
RashmiRB <rbidanta@yahoo.in>
2017-04-17 03:49:29 -0400

    Sunday Changes

--- app/src/main/java/pervasive/iu/com/greenthumb/GardenPartner/GardenOverview.java
+++ app/src/main/java/pervasive/iu/com/greenthumb/GardenPartner/GardenOverview.java
@@ -1,16 +1,22 @@
 package pervasive.iu.com.greenthumb.GardenPartner;
 
-import android.content.Intent;
+
+import android.content.DialogInterface;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -25,17 +31,23 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import pervasive.iu.com.greenthumb.Adapter.MemberListViewAdapter;
 import pervasive.iu.com.greenthumb.DBHandler.GardenInfo;
+import pervasive.iu.com.greenthumb.DBHandler.MemberInfo;
 import pervasive.iu.com.greenthumb.R;
 
 public class GardenOverview extends Fragment {
 
 
     private EditText gName,gAddress,gOwnerName,gContactNumber;
-    private Button reqMembership,cancelMembership,viewRequests;
+    private Button reqMembership,updateInfo,viewRequests;
+   // private ImageButton
     private ImageView gardenImage;
     private FirebaseAuth firebaseAuth;
     private DatabaseReference dbreference;
@@ -51,6 +63,14 @@ public class GardenOverview extends Fragment {
 
     boolean hasMemebershipRequested = false;
 
+    private FirebaseUser loggedUser;
+
+    private String gardenId = "";
+
+    private ListView mlistView;
+
+    private ArrayList<MemberInfo> memberObjectList ;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -58,10 +78,15 @@ public class GardenOverview extends Fragment {
         getActivity().setTitle(R.string.garden_overview);
         firebaseAuth = FirebaseAuth.getInstance();
 
+        loggedUser = firebaseAuth.getCurrentUser();
         currentUser = firebaseAuth.getCurrentUser().getUid().toString();
 
 
     }
+
+
+
+    List<String> memberslist;
 
     @Nullable
     @Override
@@ -69,10 +94,6 @@ public class GardenOverview extends Fragment {
 
 
         View view = inflater.inflate(R.layout.activity_garden_overview,container,false);
-
-
-
-
 
         Bundle bundle = getArguments();
 
@@ -84,14 +105,13 @@ public class GardenOverview extends Fragment {
 
         String contactNumber = gInfo.getgOwnerPhone().toString();
 
+        gardenId = gInfo.getgId().toString();
+
         String location = gInfo.getgAddress();
 
         String imagePath = gInfo.getgFireBasePath();
 
-
-
         gardenImagesRef = storage.getReference(imagePath);
-
 
         gName = (EditText) view.findViewById(R.id.g_name);
         gAddress = (EditText) view.findViewById(R.id.g_address);
@@ -99,7 +119,7 @@ public class GardenOverview extends Fragment {
         gContactNumber = (EditText) view.findViewById(R.id.g_contactNumber);
         gardenImage = (ImageView) view.findViewById(R.id.gardenOImage);
         reqMembership = (Button) view.findViewById(R.id.req_memship);
-        //cancelMembership = (Button) view.findViewById(R.id.cancel_memship);
+        updateInfo = (Button) view.findViewById(R.id.update_info);
         viewRequests = (Button) view.findViewById(R.id.view_requests);
 
 
@@ -109,11 +129,13 @@ public class GardenOverview extends Fragment {
                 .into(gardenImage);
 
         gardenImage.setVisibility(View.VISIBLE);
-       // gardenImage.setVisibility(View.INVISIBLE);
+        // gardenImage.setVisibility(View.INVISIBLE);
 
         // For fetching the name of Owner
 
         userReference = FirebaseDatabase.getInstance().getReference(gardenOwner);
+
+
 
         userReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -140,22 +162,17 @@ public class GardenOverview extends Fragment {
             }
         });
 
-
-
-
-
-
         gName.setText(gardenName);
         gAddress.setText(location);
         gContactNumber.setText(contactNumber);
         if(currentUser.equalsIgnoreCase(gardenOwner)){
 
             gName.setEnabled(true);
-            gOwnerName.setEnabled(true);
+            gOwnerName.setEnabled(false);
             gAddress.setEnabled(true);
             gContactNumber.setEnabled(true);
-            reqMembership.setVisibility(View.INVISIBLE);
-            cancelMembership.setVisibility(View.INVISIBLE);
+            reqMembership.setVisibility(View.GONE);
+            updateInfo.setVisibility(View.VISIBLE);
             viewRequests.setVisibility(View.VISIBLE);
         }else{
 
@@ -168,6 +185,116 @@ public class GardenOverview extends Fragment {
             hasMembershipRequested(gInfo.getgId(),currentUser);
 
         }
+
+
+
+
+        updateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                updateInfo(gardenId,gName.getText().toString(),gAddress.getText().toString(),gContactNumber.getText().toString());
+
+            }
+
+        });
+
+
+        memberslist = new ArrayList<String>();
+
+        DatabaseReference gardenReference = FirebaseDatabase.getInstance().getReference("gardens");
+
+        gardenReference.child(gardenId).child("gMembers").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+
+                    if(Boolean.getBoolean(ds.getValue().toString()) == false){
+
+                        memberslist.add(ds.getKey());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        viewRequests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                AlertDialog.Builder builderSingle = new AlertDialog.Builder(getActivity());
+                builderSingle.setIcon(R.mipmap.ic_new_member_req);
+                builderSingle.setTitle("Pending Requests:-");
+
+
+                Bundle bundle = new Bundle();
+                LayoutInflater inflater = getLayoutInflater(bundle);
+                View memberListView = inflater.inflate(R.layout.view_membership_request, null);
+
+                memberObjectList  = new ArrayList<MemberInfo>();
+
+                for(String member: memberslist){
+
+                    System.out.println("Member: "+member);
+
+                    memberObjectList.add(getMemberInfo(member.toString()));
+
+
+                }
+
+
+                System.out.println("memberObjectList----------"+memberObjectList);
+
+                mlistView = (ListView) memberListView.findViewById(R.id.newMemberList);
+
+
+                final MemberListViewAdapter memberAdapter = new MemberListViewAdapter(memberObjectList,getContext());
+
+                mlistView.setAdapter(memberAdapter);
+
+
+
+
+
+
+
+
+
+                builderSingle.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builderSingle.setAdapter(memberAdapter, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String strName = memberAdapter.getItem(which).getFirstname()+ " " + memberAdapter.getItem(which).getFirstname();
+                        AlertDialog.Builder builderInner = new AlertDialog.Builder(getActivity());
+                        builderInner.setMessage(strName);
+                        builderInner.setTitle("Your Selected Item is");
+                        builderInner.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog,int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        builderInner.show();
+                    }
+                });
+                builderSingle.show();
+            }
+        });
 
 
 
@@ -216,11 +343,41 @@ public class GardenOverview extends Fragment {
 
 
 
+
+
+
+
+
     @Override
     public void onStart() {
         super.onStart();
     }
 
+
+   private void updateInfo(String gardenId , String gardenName, String gardenAddress, String contactPhone){
+
+       DatabaseReference gardenReference = FirebaseDatabase.getInstance().getReference("gardens");
+
+       if(isDataModified()) {
+           /*gardenReference.child(gardenId).child("gName").setValue(gName.getText());
+           gardenReference.child(gardenId).child("gAddress").setValue(gAddress.getText());
+           gardenReference.child(gardenId).child("gOwnerPhone").setValue(gContactNumber.getText());*/
+           Toast.makeText(getActivity(),"Information Updated Successfully!",Toast.LENGTH_LONG).show();
+       }else {
+           Toast.makeText(getActivity(),"Please provide valid information!",Toast.LENGTH_LONG).show();
+       }
+
+   }
+
+    private boolean isDataModified(){
+
+        if(gName.getText().toString().isEmpty() || gAddress.getText().toString().isEmpty() || gContactNumber.getText().toString().isEmpty()){
+
+            return false;
+
+        }
+        return true;
+    }
 
 
     private void requestMembership(String gardenId , String requesterId){
@@ -271,6 +428,63 @@ public class GardenOverview extends Fragment {
             }
         });
     }
+
+
+
+
+
+    public MemberInfo getMemberInfo(String memberId){
+
+        final MemberInfo memberInfo = new MemberInfo();
+
+        System.out.println("User GUID:"+memberId);
+
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference(memberId);
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                memberInfo.setUserId(dataSnapshot.getKey());
+
+                System.out.println("User Id:"+dataSnapshot.getKey());
+
+                for(DataSnapshot memberds : dataSnapshot.getChildren()){
+
+
+
+                    if(memberds.getKey().equalsIgnoreCase("firstname")){
+
+
+
+                        System.out.println("First Name:"+memberds.getValue().toString());
+
+                    }else if(memberds.getKey().equalsIgnoreCase("lastname")){
+
+                        //memberInfo.setLastname(memberds.getValue().toString());
+                        System.out.println("First Name:"+memberds.getValue().toString());
+
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        return memberInfo;
+    }
+
+
+
+
+
+
+
 
 
 
